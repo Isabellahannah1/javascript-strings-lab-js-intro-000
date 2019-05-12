87f3d4c29b69f35dd9b7bf3fc6cf694dc5b965b8
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-12 15:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var conversation = "topic"
+var topic = 'the weather'
 
-`Let's talk about ${topic}.`
+var conversation = `Let's talk about ${topic}.`
